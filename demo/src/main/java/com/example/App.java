dc9af3acd315b15b08808b798495f000ffcbf36b
MNht12 <s3924871@rmit.edu.vn>
2023-04-26 22:03:53 +0700

    AA

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -5,7 +5,6 @@ import java.util.stream.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 /**
--- demo/src/main/java/com/example/ShoppingCart.java
+++ demo/src/main/java/com/example/ShoppingCart.java
@@ -1,9 +1,15 @@
 package com.example;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
 public class ShoppingCart {
     private List<String> cart;
     private Set<Product> productSet;
+
+    public ShoppingCart(Set<Product> productSet) {
+        this.productSet = productSet;
+        this.cart = new ArrayList<String>();
+    }
 }
